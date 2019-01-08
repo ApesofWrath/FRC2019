db708ecf57994bbe054bd43c53da1f94c7e4e80a
Kieren Emens <kierenemens@gmail.com>
2019-01-07 21:00:10 -0800

    added basic skeleton

---
+++ src/main/cpp/suction.cpp
@@ -0,0 +1,38 @@
+/*
+    suction.cpp
+    Created on: Jan 7th 2019
+*/
+
+#include <WPILib.h>
+
+
+/*Variables + constants*/
+
+const int INIT_STATE = 0;
+const int IN_STATE = 1;
+const int OUT_STATE = 2;
+
+
+Suction::Suction() {
+
+}
+
+void Suction::Pull() {
+
+}
+
+void Suction:Push() {
+
+}
+
+void Suction:SuctionStateMachine() {
+    switch:
+        case INIT_STATE:
+            break;
+        case PULL_STATE:
+            break;
+        case PUSH_STATE:
+            break;
+        case SWITCH_STATE:
+            break;
+}
