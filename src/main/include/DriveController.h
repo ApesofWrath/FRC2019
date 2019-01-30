484e19c0f54eea700dd116aa37662a1fb5c70efb
Grace0 <gracekwak25@gmail.com>
2019-01-29 23:41:29 -0800

    switch talon ids

--- src/main/include/DriveController.h
+++ src/main/include/DriveController.h
@@ -7,7 +7,8 @@ class DriveController : public DriveBase {
 public:
 
 	//l1, l2, l3, l4, r1, r2, r3, r4, pcm, f_channel, r_channel, two_speed
-	DriveController() : DriveBase(21, 5, 8, 0, 24, 6, 7, 0, 0, 0, 0, false) {
+	//left right from robot perspective
+	DriveController() : DriveBase(24, 6, 7, 0, 21, 5, 8, 0, 0, 0, 0, false) {
 
 
 	}
