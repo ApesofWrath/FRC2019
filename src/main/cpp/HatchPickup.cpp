93bd72ae4e491db33f35c07237bdc0c93ea775e9
superspeeder <andy@delusion.org>
2019-02-02 15:36:51 -0800

    Added solenoid state machine:

--- src/main/cpp/HatchPickup.cpp
+++ src/main/cpp/HatchPickup.cpp
@@ -3,10 +3,13 @@
 const int PULL_STATE = 0;
 const int PUSH_STATE = 1;
 
+const int DOWN_STATE = 0;
+const int UP_STATE = 1;
+
 HatchPickup::HatchPickup() {
 
   suction = new frc::DigitalOutput(SUCTION_CHANNEL);
-
+  solenoid = new frc::DoubleSolenoid(SOLENOID_FORWARD_CHANNEL, SOLENOID_REVERSE_CHANNEL);
 }
 
 void HatchPickup::Pull() {
@@ -18,6 +21,18 @@ void HatchPickup::Pull() {
 void HatchPickup::Push() {
 
   suction->Set(false);
+
+}
+
+void HatchPickup::Up() {
+
+  solenoid->Set(frc::DoubleSolenoid::kForward);
+
+}
+
+void HatchPickup::Down() {
+
+  solenoid->Set(frc::DoubleSolenoid::kReverse);
 
 }
 
@@ -33,4 +48,20 @@ void HatchPickup::SuctionStateMachine() {
             frc::SmartDashboard::PutString("SUCTION", "push");
             break;
     }
+}
+
+
+void HatchPickup::SolenoidStateMachine() {
+
+  switch (solenoid_state) {
+    case DOWN_STATE:
+      Down();
+      frc::SmartDashboard::PutString("SOLENOID", "down");
+      break;
+
+    case UP_STATE:
+      Up();
+      frc::SmartDashboard::PutString("SOLENOID", "up");
+      break;
+  }
 }
